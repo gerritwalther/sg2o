a00556fcb2e22cb4e09c8ae359f471144096edd5
Gerrit Walther <gerritwalther@gmail.com>
2015-02-28 18:54:23 +0100

    Added cronjob and repo to server. Currently building once per hour. closes #53

--- sg2o-install.user.js
+++ sg2o-install.user.js
@@ -2,7 +2,7 @@
 // @name          SG2O
 // @namespace     sg2o
 // @description   Bring old functions from sg+ back to SGv2.
-// @copyright     2014+, Clerius (https://github.com/gerritwalther)
+// @copyright     2014+, Clerius (https://sg2o.clerius.de)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @version       0.1
 //
@@ -14,6 +14,6 @@
 (function () {
   var sg2o = document.createElement('script');
   sg2o.type = 'text/javascript';
-  sg2o.src = 'https://raw.githack.com/gerritwalther/sg2o/master/sg2o.user.js';
+  sg2o.src = 'http://sg2o.clerius.de/sg2o.user.js';
   document.body.appendChild(sg2o);
 })();
--- userscript-Header.js
+++ userscript-Header.js
@@ -2,7 +2,7 @@
 // @name          SG2O
 // @namespace     sg2o
 // @description   Bring old functions from sg+ back to SGv2.
-// @copyright     2014+, Clerius (https://github.com/gerritwalther)
+// @copyright     2014+, Clerius (https://sg2o.clerius.de)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @version       0.1
 //
