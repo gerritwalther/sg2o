4e1b6556001ab7f7c2af50458ae2fc9e3db213e4
Gerrit Walther <gerritwalther@gmail.com>
2015-03-05 10:20:54 +0100

    Added ssl to the server to fix the auto update function.

--- sg2o-install.user.js
+++ sg2o-install.user.js
@@ -2,14 +2,14 @@
 // @name          SG2O
 // @namespace     sg2o
 // @description   Bring old functions from sg+ back to SGv2.
-// @copyright     2014+, Clerius (http://sg2o.clerius.de)
+// @copyright     2014+, Clerius (https://sg2o.clerius.de)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       0.1.1
+// @version       0.1.2
 //
 // @include   http://www.steamgifts.com/*
 //
-// @downloadURL http://sg2o.clerius.de/sg2o.user.js
-// @updateURL http://sg2o.clerius.de/sg2o.meta.js
+// @downloadURL https://sg2o.clerius.de/sg2o.user.js
+// @updateURL https://sg2o.clerius.de/sg2o.meta.js
 //
 // @run-at    document-end
 //
