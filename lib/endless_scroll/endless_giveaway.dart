0ba5ba0aac822dad07265f3f96d502100b513e13
Gerrit Walther <gerritwalther@gmail.com>
2015-03-28 13:59:38 +0100

    Fixes #66 by adding the type of giveaway page to the request parameter.

--- lib/endless_scroll/endless_giveaway.dart
+++ lib/endless_scroll/endless_giveaway.dart
@@ -2,14 +2,22 @@ part of sg2o;
 
 class EndlessGiveAway extends EndlessScroll {
 
+    String type = '';
+
     /// Constructor for endless scroll on giveaway page. Specifies the gridViewContainer as [itemsContainer].
     EndlessGiveAway() : super() {
+        String currentUrl = document.baseUri;
+        if (currentUrl.contains('type')) {
+            /// http://www.steamgifts.com/giveaways/search?page=2&type=wishlist
+            type = currentUrl.split('type=').last;
+            window.console.log('Page type is $type');
+        }
         itemsContainer = querySelector('.$classGridViewContainer');
     }
 
     void updatePage(num page) {
         updatePagination('Loading next page  ', true);
-        HttpRequest.request('$urlGiveAwayPage$nextPage').then((HttpRequest resp) {
+        HttpRequest.request('$urlGiveAwayPage$nextPage&type=$type').then((HttpRequest resp) {
             Document nextPageDocument = domParser.parseFromString(resp.responseText, 'text/html');
             ElementList pagination = nextPageDocument.querySelector('.$classPagination').children;
             ElementList paginator = nextPageDocument.querySelectorAll('.$classPaginationNavigation>a');
