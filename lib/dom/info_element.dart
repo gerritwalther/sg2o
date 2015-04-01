0a819e57254e9f0e04f580668567561b8d7703be
Gerrit Walther <gerritwalther@gmail.com>
2015-04-01 20:10:17 +0200

    Cleaned up infoElement

--- lib/dom/info_element.dart
+++ lib/dom/info_element.dart
@@ -2,13 +2,10 @@ part of sg2o;
 
 class InfoElement extends DivElement {
 
-    DivElement element;
-
     factory InfoElement(String text) {
         DivElement element = new DivElement();
         element
             ..text = text
-            ..setAttribute('is', 'my-info-div')
         ;
         new Timer(const Duration(seconds: 3), () => element.remove());
         return element;
