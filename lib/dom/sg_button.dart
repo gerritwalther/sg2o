a7b23615ad9b8d9d1f5dcc8450a8c0a84e758a67
Gerrit Walther <gerritwalther@gmail.com>
2015-04-03 12:40:43 +0200

    Fixed missing refresh icon on resync button. #65

--- lib/dom/sg_button.dart
+++ lib/dom/sg_button.dart
@@ -4,12 +4,18 @@ class SGButton extends DivElement {
 
     factory SGButton(String faClass, String text, fnButtonListener) {
         DivElement element = new DivElement();
-        element
+        Element icon = new Element.tag('i');
+
+        icon
             ..classes.add('fa')
             ..classes.add(faClass)
+        ;
+
+        element
             ..classes.add('form__submit-button')
             ..style.setProperty('margin-top', '10px')
-            ..text = ' $text'
+            ..append(icon)
+            ..appendText(' $text')
             ..onClick.listen(fnButtonListener)
         ;
         return element;
