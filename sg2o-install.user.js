3429543a376c8fcb89233a94082b3700985a36bf
Gerrit Walther <gerritwalther@gmail.com>
2015-04-08 17:06:32 +0200

    Added homepage url. Closes #77

--- sg2o-install.user.js
+++ sg2o-install.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name          SG2O
 // @namespace     sg2o
+// @homepage      http://sg2o.clerius.de
 // @description   Bring old functions from sg+ back to SGv2.
 // @copyright     2014+, Clerius (https://sg2o.clerius.de)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
