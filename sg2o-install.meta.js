e24837ab3a189ad4b2155aadd8b3cf8dd7e49b5c
Gerrit Walther <gerritwalther@gmail.com>
2015-04-17 22:06:49 +0200

    Final fix to broken pips slider.

--- sg2o-install.meta.js
+++ sg2o-install.meta.js
@@ -13,9 +13,9 @@
 // @updateURL https://sg2o.clerius.de/sg2o.meta.js
 // @supportURL https://github.com/gerritwalther/sg2o/issues
 //
+// @require https://sg2o.clerius.de/jquery/jquery-ui-slider-pips.min.js
 // @require https://code.jquery.com/jquery-2.1.3.min.js
 // @require https://code.jquery.com/ui/1.11.4/jquery-ui.min.js
-// @require https://sg2o.clerius.de/jquery/jquery-ui-slider-pips.min.js
 //
 // @run-at    document-end
 //
